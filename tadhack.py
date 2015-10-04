97478da17da974f25e9c0173437de0ee76bea676
Ubuntu <ubuntu@ip-172-31-38-223.ec2.internal>
2015-10-04 11:59:18 +0000

    Added code for tropo indexing

--- tadhack.py
+++ tadhack.py
@@ -27,6 +27,19 @@ def index_music():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+
+@app.route('/indextropo', methods=['POST'])
+def index_tropo():
+    uploaded_files = request.files.getlist("filename")
+    filenames = []
+    for file in uploaded_files:
+        print(file)
+        if file :
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filenames.append(filename)
+    return jsonify({'status':'success'})
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("2000"), debug=True)
 
