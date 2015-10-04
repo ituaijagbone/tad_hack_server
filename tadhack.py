45c78b4d2d6c72fa9eafce623f2efbc798ecb60d
Itua <i.ijagbone@gmail.com>
2015-10-04 15:41:31 +0000

    Testing clarify

--- tadhack.py
+++ tadhack.py
@@ -1,15 +1,26 @@
-import os
+import os, sys
 from flask import Flask, request, redirect, url_for, jsonify, send_from_directory
 from werkzeug import secure_filename
 
+from clarify_python import clarify
+
 UPLOAD_FOLDER = '/home/ubuntu/tadhack/music'
+HOST_URL = "http://ec2-54-86-44-93.compute-1.amazonaws.com:2000/"
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+clarify.Client('aQjWREdRGS5OZ1hj+o2RVzgM7oKuHzhRbu2GI8E7rid9Q')
+
+def send_to_clarify(media_name, media_url):
+    client.create_bundle(name=media_name,
+        media_url=media_url)
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
 
 @app.route('/indexmusic', methods=['POST'])
 def index_music():
@@ -18,9 +29,13 @@ def index_music():
     for file in uploaded_files:
         print(file)
         if file :
-            filename = secure_filename(file.filename)
+            filename = 'l_' + secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            filenames.append(filename)
+            filenames.append({'filename': filename, 'url': HOST_URL + 'music/' + filename})
+    
+    for item in filenames:
+        send_to_clarify(item['filename'], item['url'])
+
     return jsonify({'status':'success'})
 
 @app.route('/music/<filename>')
@@ -35,10 +50,45 @@ def index_tropo():
     for file in uploaded_files:
         print(file)
         if file :
-            filename = secure_filename(file.filename)
+            filename = 's_' + secure_filename('Journal_' + datetime.datetime.now().strftime("%B %d, %Y %I:%M:%S%p"))
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            filenames.append(filename)
+            filenames.append({'filename': filename, 'url': HOST_URL + 'music/' + filename})
+
+    for item in filenames:
+        send_to_clarify(item['filename'], item['url'])
+    
     return jsonify({'status':'success'})
+
+@app.route('/callaudio', methods=['POST'])
+def call_audio():
+    print(request.form)
+    return jsonify({'status':'success'})
+
+@app.route('/search', methods=['POST'])
+def search_clarify():
+    query = request.form['query']
+    result = client.search(query=query)
+    items = result['_links']['items']
+    
+    index = 0
+    hits = []
+    for item in items:
+        bundle = client.get_bundle(item['href'])
+        location = bundle[:1]
+        if location == 'l':
+            hits[index] = {'filename': bundle[2:], 'type': 'l'}
+        else: 
+            hits[index] = {'filename': bundle[2:], 'type': 's'}
+        ++index
+    
+    return jsonify({'result':hits})
+
+@app.route('/journals', methods=['GET'])
+def get_journals():
+    dirs = os.listdir(UPLOAD_FOLDER)    
+    files = [f[2:] for f in dirs if f[:1] == 's']
+    count = len(files)
+    return jsonify({'count':count, 'files': files})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("2000"), debug=True)
